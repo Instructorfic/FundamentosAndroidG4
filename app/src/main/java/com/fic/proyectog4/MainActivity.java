091d41218fe835289890c41a7dd2d4a84094dea2
Alexis Moises Montaño Araujo <alexis.montano@info.uas.edu.mx>
2022-11-03 18:42:53 -0700

    Sesión 03-11-2022

--- app/src/main/java/com/fic/proyectog4/MainActivity.java
+++ app/src/main/java/com/fic/proyectog4/MainActivity.java
@@ -6,9 +6,30 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etNombreUsuario, etContrasenia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+
+        //Variable de tipo botón y edittext
+        Button btnIniciarSesion = findViewById(R.id.btnLogin);
+        etNombreUsuario = findViewById(R.id.etNombreUsuario);
+        etContrasenia = findViewById(R.id.etContrasenia);
+
+        //Evento clic del botón btnIniciarSesión
+        btnIniciarSesion.setOnClickListener(accionBotonIniciarSesion);
+
+    }
+
+    public View.OnClickListener accionBotonIniciarSesion = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mostrarMensaje(etNombreUsuario.getText().toString());
+        }
+    };
+
+    public void mostrarMensaje(String mensaje){
+        Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
     }
 }
